748e66d38c50d9347c0a496c91d80fcd2380a90f
Teacher Du <penndu@outlook.com>
2025-03-06 13:09:49 +0800

    a

--- themes/volantis/scripts/filters/replace.js
+++ themes/volantis/scripts/filters/replace.js
@@ -2,14 +2,17 @@
 
 'use strict';
 
+// 注册 after_render:html 过滤器，用于替换文本
 hexo.extend.filter.register('after_render:html', function(data) {
+  // 检查主题配置中是否有 replace 配置项
   if (hexo.theme.config.replace) {
-    hexo.theme.config.replace.forEach(e => {
-      let s = e.split(" => ")
-      let a = s[0]
-      let b = s[1]
-      data = data.replace(new RegExp(a,"g"), b);
+    // 遍历 replace 配置项数组
+    hexo.theme.config.replace.forEach((e) => {
+      // 将替换规则按 " => " 分割为源字符串和目标字符串
+      const [a, b] = e.split(" => ");
+      // 使用正则表达式全局替换 data 中的文本
+      data = data.replace(new RegExp(a, "g"), b);
     });
   }
   return data;
-},999999999999);
+}, 999999999999);
