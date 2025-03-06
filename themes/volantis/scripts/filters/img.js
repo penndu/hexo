03bcc036a3cd32e777d76c1bd5edec5dd5673b0f
Teacher Du <penndu@outlook.com>
2025-03-06 13:19:47 +0800

    a

--- themes/volantis/scripts/filters/img.js
+++ themes/volantis/scripts/filters/img.js
@@ -2,7 +2,11 @@
 
 'use strict';
 
+// 注册 after_post_render 过滤器，用于处理图片标签
 hexo.extend.filter.register('after_post_render', function(data) {
-  data.content = data.content.replace(/<p><img src="(.*?)" alt="(.*?)"\/><\/p>/g, '<div class="img-wrap"><div class="img-bg"><img class="img" src="$1" alt="$2"\/><\/div><span class="image-caption">$2<\/span><\/div>');
+  // 替换图片标签的正则表达式，将 <p><img> 标签替换为自定义的图片包装结构
+  const imgRegex = /<p><img src="(.*?)" alt="(.*?)"\/><\/p>/g;
+  const replacement = '<div class="img-wrap"><div class="img-bg"><img class="img" src="$1" alt="$2"\/><\/div><span class="image-caption">$2<\/span><\/div>';
+  data.content = data.content.replace(imgRegex, replacement);
   return data;
 });
